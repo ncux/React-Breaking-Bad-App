489169c7c9f4f252752e6252148e45e0699bbb96
ncux <malabatalent@gmail.com>
2020-07-08 13:44:44 +0800

    1st commit

---
+++ src/components/header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import classes from './header.module.css';
+
+export const Header = props => {
+
+    return (
+        <header className={`${classes.header}`}>
+
+        </header>
+    );
+
+};
+
